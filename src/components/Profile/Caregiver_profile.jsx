7adf34b39552dee8c424be1b7921f9d8ecdef58e
NybergViktor <viktor.nyberg00@gmail.com>
2024-12-30 18:19:56 +0100

    profiles

---
+++ src/components/Profile/Caregiver_profile.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Footer from "../footer/Footer";
+import "./Profile.css";
+import { useAuth } from "../../hooks/useAuth";
+
+const CaregiverProfile = () => {
+  const [personalInformation, setPersonalInformation] = useState([]);
+  const [role, setRole] = useState("Patient: ");
+  const {
+    authState: { user },
+  } = useAuth();
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/user/find/${user}`,
+          {
+            withCredentials: true,
+          }
+        );
+        setPersonalInformation(response.data);
+        if (response.data.roles.includes("ADMIN")) {
+          setRole("Dr: ");
+        }
+      } catch (error) {
+        setPersonalInformation("Something went wrong, try again later.");
+      }
+    };
+    checkAuth();
+  }, []);
+
+  return (
+    <div className="profileMain">
+      <h1>Personal Profile</h1>
+      <div className="profileMain1">
+        <img src="src\assets\ppic.png" alt="profile picture doctor" />
+        <p>
+          {role} {user}
+        </p>
+        <h3>{role === "Dr: " ? "DOCTOR" : "PATIENT"}</h3>
+      </div>
+      <div className="profileMain2">
+        <h4>Personal Information:</h4>
+        <p>First Name: {personalInformation.firstName}</p>
+        <p>Last Name: {personalInformation.lastName}</p>
+        <p>Email: {personalInformation.mail}</p>
+      </div>
+      <Footer userType="doctor" />
+    </div>
+  );
+};
+export default CaregiverProfile;
