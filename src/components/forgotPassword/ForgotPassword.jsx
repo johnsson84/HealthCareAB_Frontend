ccbd90c8b8f73b09642a98ed7c69a283213be61e
[john gunnarsson] <john.gunnarsson@unipower.se>
2025-01-08 17:29:43 +0100

    lagt till knapp f;r att backa till login fr[n forgot password page

--- src/components/forgotPassword/ForgotPassword.jsx
+++ src/components/forgotPassword/ForgotPassword.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
 const ForgotPasswordContainer = styled.div`
   display: flex;
@@ -9,6 +10,27 @@ const ForgotPasswordContainer = styled.div`
   flex-direction: column;
 `;
 const ResetPasswordButton = styled.button`
+  cursor: pointer;
+  padding: 10px 30px;
+  background-color: #057d7a;
+  border-radius: 10px;
+  font-size: 18px;
+  font-weight: 600;
+  color: #fff;
+  margin-top: 20px;
+  transition: background-color 0.3s ease, transform 0.2s ease,
+    box-shadow 0.2s ease;
+  text-align: center;
+  border: none;
+
+  &:hover {
+    background-color: #2fadaa;
+    transform: translateY(-3px);
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.15);
+  }
+`;
+
+const ReturnToLoginButton = styled.button`
   cursor: pointer;
   padding: 10px 30px;
   background-color: #057d7a;
@@ -68,15 +90,14 @@ const ForgotPassword = () => {
         case 403:
           return "Not Allowed: You are not gruanted access.";
         case 404:
-            return "Email not found. Check email and try again";      
+          return "Email not found. Check email and try again";
         default:
           return "Error sending the reset link. Please try again later.";
       }
     } else {
-      return "An unexpected error occurred. Please try again later."
+      return "An unexpected error occurred. Please try again later.";
     }
   };
-
 
   const handlePasswordForgotten = async (e) => {
     e.preventDefault();
@@ -90,12 +111,11 @@ const ForgotPassword = () => {
     }
 
     try {
-      setLoading(true); 
+      setLoading(true);
       await axios.post(`${import.meta.env.VITE_API_URL}/auth/forgot-password`, {
         email: email.trim(), // tar bort mellanslag
       });
       setMessage("The reset link was sent! Please check your email.");
-
     } catch (err) {
       console.error(err);
       setMessage(errorHandler(err));
@@ -116,12 +136,19 @@ const ForgotPassword = () => {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-        <ResetPasswordButton type="submit" disabled={loading}>{loading ? "Sending..." : "Send Reset Link"}</ResetPasswordButton>
+        <ResetPasswordButton type="submit" disabled={loading}>
+          {loading ? "Sending..." : "Send Reset Link"}
+        </ResetPasswordButton>
         {message && (
           <p style={{ color: error ? "red" : "green", marginTop: "10px" }}>
             {message}
           </p>
         )}
+        <ReturnToLoginButton>
+          <Link className="link" to="/login">
+            return to login
+          </Link>
+        </ReturnToLoginButton>
       </FormWrapper>
     </ForgotPasswordContainer>
   );
