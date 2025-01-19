7f3c31bb6a10fe4413fc3763a7c8ccc276bef6a9
[john gunnarsson] <john.gunnarsson@unipower.se>
2025-01-19 18:23:41 +0100

    trying to extract coworker id from my fetch and save it to a state

--- src/components/facility/Facility.jsx
+++ src/components/facility/Facility.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 const Facility = () => {
   const [hospitals, setHospitals] = useState([]);
-
+  const [coworkerDetails, setCoworkerDetails ] = useState(null);
   // Testa att göra en lista med alla saker man kan skriva och sen kolla mot den.
 
   // hämtar alla sjukhus från DB
@@ -16,12 +16,25 @@ const Facility = () => {
         }
       );
       setHospitals(response.data);
+
+
+      // plockar ut coworker ids 
+      const coworkerIds = [...new setHospitals(response.data.flatMap((i) => i.coworkersId))];
+
     } catch (err) {
       console.log("error fetching facility " + err);
     }
   };
+  
+  // Hämtar coworker info via deras ID
+  // const fetchCoworkerDetails = async () => {
+  //   try {
+  //     const res = await axios.get(
+  //       `${import.meta.env.VITE_API_URL}/user/find/{username}`
 
-  console.log(hospitals);
+  //     )
+  //   }
+  // }
 
   useEffect(() => {
     fetchHospitals();
@@ -33,38 +46,38 @@ const Facility = () => {
       {hospitals.length === 0 ? (
         <p> no hospitals available</p>
       ) : (
-        <ul style={{border: "1px solid black ", display: "flex", justifyItems: "center", flexDirection: "column"}}>
+        <ul
+          style={{
+            border: "1px solid black ",
+            display: "flex",
+            justifyItems: "center",
+            flexDirection: "column",
+          }}
+        >
           {hospitals.map((hospitals, index) => (
-            <div key={index} >
-            <li>Hospital: {hospitals.facilityName} <br />
-            Phone: {hospitals.phoneNumber} <br />
-            Mail: {hospitals.email} <br />
-            Open hours: {hospitals.hoursOpen}</li>
-              
-              {hospitals.address ? (
-              <>
+            <div key={index}>
               <li>
-              <strong>Street:</strong> {hospitals.address.street} <br />
-              <strong>City:</strong> {hospitals.address.city} <br />
-              <strong>Postal Code:</strong>{hospitals.address.postcode} <br />
-              <strong>Region:</strong> {hospitals.address.region} <br />
-              <strong>Country:</strong> {hospitals.address.country} <br />
+                Hospital: {hospitals.facilityName} <br />
+                Phone: {hospitals.phoneNumber} <br />
+                Mail: {hospitals.email} <br />
+                Open hours: {hospitals.hoursOpen} <br />
+                Address: {hospitals.address.street} <br />
+                City: {hospitals.address.city} <br />
+                Region: {hospitals.address.region} <br />
+                Country: {hospitals.address.country}
               </li>
-              </>
-            ) : (
-              "Address not available"
-            )}
-            {hospitals.coworkersId && hospitals.coworkersId.length > 0 ? (
-              <ul>
-                {hospitals.coworkersId.map((coworkerId, i) => (
-                  <li key={i}>{coworkerId}</li>
-                ))}
-              </ul>
-            ) : (
-              "Nobody seems to work here??"
-            )}
-            
-            
+
+              
+              {hospitals.coworkersId && hospitals.coworkersId.length > 0 ? (
+                <ul>
+                  
+                  {hospitals.coworkersId.map((coworkerId, i) => (
+                    <li key={i}>Doctor: {coworkerId}</li>
+                  ))}
+                </ul>
+              ) : (
+                "Nobody seems to work here??"
+              )}
             </div>
           ))}
         </ul>
